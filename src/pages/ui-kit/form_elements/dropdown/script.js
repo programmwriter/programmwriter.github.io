c6166bd497d50cf84a0b621be51efdf34414dbab
programmwriter <programmwriter@mail.ru>
2019-07-10 14:20:00 +0500

    ddItem mixin + loop

--- src/pages/ui-kit/form_elements/dropdown/script.js
+++ src/pages/ui-kit/form_elements/dropdown/script.js
@@ -1,22 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const ddArrowAll = document.querySelectorAll(".dropdown__arrowBox");
 
+  //обработчик появления списка
   ddArrowAll.forEach(function(element) {
     element.addEventListener("click", () => {
       const ddList = element.parentNode.nextSibling;
       const ddInput = element.parentNode;
       if (ddList.style.display === "block") {
         ddList.style.display = "none";
-        // ddInput.classList.remove(".dropdown_clicked");
-        ddInput.className = ".dropdown__input";
+        ddInput.className = "dropdown__input";
       } else {
         ddList.style.display = "block";
-        ddInput.className = ".dropdown__input_clicked";
+        ddInput.className = "dropdown__input_clicked";
       }
-
-      // ddList.style.display === "block"
-      //   ? (ddList.style.display = "none")
-      //   : (ddList.style.display = "block");
     });
   });
+
+  //обработчик нажатия кнопок + - в списке
+  // let opt = {};
 });
