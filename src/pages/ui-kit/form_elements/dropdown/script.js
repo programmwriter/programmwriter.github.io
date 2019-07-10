5e4e15cd20e0cdc3a6da4c4d257933a55430f5ad
programmwriter <programmwriter@mail.ru>
2019-07-10 22:18:24 +0500

    dd input text change

--- src/pages/ui-kit/form_elements/dropdown/script.js
+++ src/pages/ui-kit/form_elements/dropdown/script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
     element.addEventListener("click", evt => {
       const ddItemBtnText = evt.target.parentNode.previousSibling.innerText;
       ddItemObj[ddItemBtnText]--;
-      //удаляем клас актив кнопке минус и добавляем атрибут disabled
+      // клас актив кнопке минус и удаляем атрибут disabled
       if (ddItemObj[ddItemBtnText] === 0) {
         evt.target.classList.remove("ddBtn_active");
         evt.target.setAttribute("disabled", "disabled");
@@ -60,8 +60,39 @@ document.addEventListener("DOMContentLoaded", () => {
         );
       }
       evt.target.previousSibling.innerText = String(ddItemObj[ddItemBtnText]);
-      console.log(ddItemObj);
+      let ddInputText =
+        evt.target.parentNode.parentNode.parentNode.previousSibling;
+      evt.target.parentNode.parentNode.parentNode.previousSibling.firstChild = ddString(
+        ddItemObj
+      );
+      console.log(ddInputText);
+      console.log(ddString(ddItemObj));
     });
   });
   console.log(ddBtnMinus);
 });
+function ddString(obj) {
+  let arr = [];
+  for (let key in obj) {
+    arr.push(` ${obj[key]} ${key}`);
+  }
+  return arr.join();
+}
+function num2str(n, text_forms) {
+  n = Math.abs(n) % 100;
+  var n1 = n % 10;
+
+  if (n > 10 && n < 20) {
+    return text_forms[2];
+  }
+
+  if (n1 > 1 && n1 < 5) {
+    return text_forms[1];
+  }
+
+  if (n1 == 1) {
+    return text_forms[0];
+  }
+
+  return text_forms[2];
+}
