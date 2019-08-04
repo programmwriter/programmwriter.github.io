8bfad4eaac85fa14f16795d9ea8ba395e54dddf2
programmwriter <programmwriter@mail.ru>
2019-08-04 12:49:49 +0500

    Закончил range slider

--- src/pages/ui-kit/form_elements/range_slider/rangeSlider.js
+++ src/pages/ui-kit/form_elements/range_slider/rangeSlider.js
@@ -4,22 +4,36 @@ $(document).ready(function() {
       range: true,
       min: 0,
       max: 15000,
+      step: 5000,
       classes: {
-        "ui-slider": "track",
-        "ui-slider-handle": "handle"
+        "ui-slider": "rangeSlider__track",
+        "ui-slider-handle": "rangeSlider__handle",
+        "ui-slider-range": "rangeSlider__range"
       },
       values: [5000, 10000],
       slide: function(event, ui) {
-        $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        $("#amount").val(
+          zero3(ui.values[0]) + "\u20bd - " + zero3(ui.values[1]) + "\u20bd"
+        );
       }
     });
 
     $("#slider-range").slider("option", "classes.ui-corner-all", null);
     $("#amount").val(
-      "$" +
-        $("#slider-range").slider("values", 0) +
-        " - $" +
-        $("#slider-range").slider("values", 1)
+      zero3($("#slider-range").slider("values", 0)) +
+        "\u20bd - " +
+        zero3($("#slider-range").slider("values", 1)) +
+        "\u20bd"
     );
   });
 });
+
+//Функция zero3() получает число и преобразует его в строку разделяя тысячи пробелом
+function zero3(val) {
+  let strVal = String(val);
+  return (
+    strVal.substring(0, strVal.length - 3) +
+    " " +
+    strVal.substring(strVal.length - 3)
+  );
+}
